1a5e80b284c7e7acb44da9487451a2d2a17d6d45
zakarianai <zakarianaim56@gmail.com>
2024-05-14 17:53:44 +0100

    front

---
+++ app-frontend/src/inscrip.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
+import axios from 'axios';
+
+function SignupForm() {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    fetchClients();
+  }, []);
+
+  const fetchClients = () => {
+    axios.get("http://localhost:8080/clients")
+      .then(response => {
+        setClients(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching clients:', error);
+      });
+  };
+  //aaaaaaaaaaaa
+  return (
+    <Container component="main" maxWidth="xs">
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th><th>NOM</th><th>PRENOM</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clients.map(client => (
+              <tr key={client.id}>
+                <td>{client.idClient}</td>
+                <td>{client.nom_utilisateur}</td>
+                <td>{client.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Container>
+  );
+}
+
+export default SignupForm;
